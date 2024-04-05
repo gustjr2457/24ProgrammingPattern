3ce0bac9895763ba8f9b5ca42ae915724b48e8c3
gustjr2457 <gustjr2457@naver.com>
2024-04-05 19:14:34 +0900

    gitIgnore수정 && lecture04패키지 포함 && Lecture05 만드는 중

---
+++ HW_Engine/Lecture04-HW/Lecture04-HW.cpp
@@ -0,0 +1,128 @@
+﻿
+#include <iostream>
+#include <GLFW/glfw3.h>
+#include <cmath>
+#pragma comment(lib, "opengl32.lib")
+
+using namespace std;
+
+
+double prevMouseX = 0.0;
+double prevMouseY = 0.0;
+bool LorR = true;
+
+
+void errorCallback(int error, const char* description)
+{
+    cerr << "GLFW Error: " << description << endl;
+}
+
+bool approximatelyEqual(double a, double b, double epsilon) 
+{
+    return fabs(a - b) <= epsilon;
+}
+
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos) 
+{
+    if (prevMouseX != 0.0 && prevMouseY != 0.0) {
+        if (approximatelyEqual(xpos, prevMouseX, 1) && approximatelyEqual(ypos, prevMouseY, 1)) {
+            if (LorR)
+                glClearColor(0.0f, 1.0f, 0.0f, 1.0f);
+            else
+                glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+        }
+        else
+        {
+            if (LorR)
+                glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
+            else
+                glClearColor(1.0f, 0.0f, 1.0f, 1.0f);
+        }
+    }
+
+    prevMouseX = xpos;
+    prevMouseY = ypos;
+}
+
+
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+
+
+void mouseCallback(GLFWwindow* window, int button, int action, int mods)
+{
+    if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
+    {
+        LorR = true;
+        glClearColor(0.0f, 1.0f, 0.0f, 1.0f);
+        glfwSetCursorPosCallback(window, mouse_callback);
+    }
+    if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_RELEASE)
+    {
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glfwSetCursorPosCallback(window, nullptr);
+    }
+    if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS)
+    {
+        LorR = false;
+        glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+        glfwSetCursorPosCallback(window, mouse_callback);
+    }
+    if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_RELEASE)
+    {
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glfwSetCursorPosCallback(window, nullptr);
+    }
+}
+
+
+int main(void)
+{
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    /* Create a windowed mode window and its OpenGL context */
+    GLFWwindow* window;
+    window = glfwCreateWindow(1280, 768, "PP_Engine", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+    glfwSetErrorCallback(errorCallback);
+    glfwSetKeyCallback(window, keyCallback);
+    glfwSetMouseButtonCallback(window, mouseCallback);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Poll for and process events */
+        glfwPollEvents();
+
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+    }
+
+    glfwTerminate();
+    return 0;
+}
---
+++ HW_Engine/Lecture05-HW/Lecture05-HW.cpp
@@ -0,0 +1,169 @@
+﻿#include <iostream>
+#include <GLFW/glfw3.h>
+#include <cmath>
+#pragma comment(lib, "opengl32.lib")
+
+using namespace std;
+
+double prevMouseX = 0.0;
+double prevMouseY = 0.0;
+bool LorR = true;
+float moveSideFactor = 0.0f;
+float moveUDFactor = 0.0f;
+float scaleFactor = 1.0f;
+
+
+void errorCallback(int error, const char* description)
+{
+    cerr << "GLFW Error: " << description << endl;
+}
+
+bool approximatelyEqual(double a, double b, double epsilon)
+{
+    return abs(a - b) <= epsilon;
+}
+
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+    
+    if (LorR)
+    {
+        if (prevMouseX - xpos > 0 && prevMouseX != 0.0)
+            moveSideFactor -= abs(prevMouseX - xpos) * 0.0015f;
+        else if (prevMouseX - xpos < 0 && prevMouseX != 0.0)
+            moveSideFactor += abs(prevMouseX - xpos) * 0.0015f;
+        if (prevMouseY - ypos > 0 && prevMouseY != 0.0)
+            moveUDFactor += abs(prevMouseY - ypos) * 0.0025f;
+        else if (prevMouseY - ypos < 0 && prevMouseY != 0.0)
+            moveUDFactor -= abs(prevMouseY - ypos) * 0.0025f;
+    }
+    else if (LorR == false)
+    {
+        if (prevMouseX - xpos > 0)
+        {
+            scaleFactor += 0.01f;
+        }
+        else if (prevMouseX - xpos < 0)
+        {
+            scaleFactor -= 0.01f;
+        }
+    }
+    
+    prevMouseX = xpos;
+    prevMouseY = ypos;
+}
+
+
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+
+
+void mouseCallback(GLFWwindow* window, int button, int action, int mods)
+{
+    if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
+    {
+        LorR = true;
+        glfwSetCursorPosCallback(window, mouse_callback);
+    }
+    if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_RELEASE)
+    {
+        prevMouseX = 0.0;
+        prevMouseY = 0.0;
+        glfwSetCursorPosCallback(window, nullptr);
+    }
+    if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS)
+    {
+        LorR = false;
+        glfwSetCursorPosCallback(window, mouse_callback);
+    }
+    if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_RELEASE)
+    {
+        prevMouseX = 0.0;
+        prevMouseY = 0.0;
+        glfwSetCursorPosCallback(window, nullptr);
+    }
+}
+
+int render()
+{
+    glBegin(GL_TRIANGLES);
+
+    glColor3f(1.0f, 1.0f, 1.0f);
+    glVertex2f((-0.45f + moveSideFactor), (0.0f + moveUDFactor));
+
+    glColor3f(1.0f, 1.0f, 1.0f);
+    glVertex2f((- 0.10f + moveSideFactor), (0.0f + moveUDFactor));
+
+    glColor3f(1.0f, 1.0f, 1.0f);
+    glVertex2f((-0.28f + moveSideFactor), (-0.24f + moveUDFactor));
+    glEnd();
+    
+
+
+    glBegin(GL_TRIANGLES);
+
+    glColor3f(1.0f, 1.0f, 1.0f);
+    glVertex2f((-0.27f + moveSideFactor), (0.22f + moveUDFactor));
+
+    glColor3f(1.0f, 1.0f, 1.0f);
+    glVertex2f((-0.39f + moveSideFactor), (-0.40f + moveUDFactor));
+
+    glColor3f(1.0f, 1.0f, 1.0f);
+    glVertex2f((-0.21f + moveSideFactor), (-0.15f + moveUDFactor));
+    glEnd();
+
+
+    glBegin(GL_TRIANGLES);
+
+    glColor3f(1.0f, 1.0f, 1.0f);
+    glVertex2f((-0.45f + moveSideFactor), (0.0f + moveUDFactor));
+
+    glColor3f(1.0f, 1.0f, 1.0f);
+    glVertex2f((-0.23f + moveSideFactor), (0.0f + moveUDFactor));
+
+    glColor3f(1.0f, 1.0f, 1.0f);
+    glVertex2f((-0.15f + moveSideFactor), (-0.40f + moveUDFactor));
+    glEnd();
+    
+
+    return 0;
+}
+
+int main(void)
+{
+    if (!glfwInit())
+        return -1;
+
+
+    GLFWwindow* window;
+    window = glfwCreateWindow(1280, 768, "PP_Engine", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+
+
+    glfwMakeContextCurrent(window);
+    glfwSetErrorCallback(errorCallback);
+    glfwSetKeyCallback(window, keyCallback);
+    glfwSetMouseButtonCallback(window, mouseCallback);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+
+        glClear(GL_COLOR_BUFFER_BIT);
+        render();
+        glfwSwapBuffers(window);
+
+    }
+
+    glfwTerminate();
+    return 0;
+}
